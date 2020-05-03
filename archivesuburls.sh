11375e6c6818b308b941bd6ea948578c9b93d3c5
osamahamad <59566963+osamahamad@users.noreply.github.com>
2020-05-04 00:02:55 +0300

    Update archivesuburls.sh

--- archivesuburls.sh
+++ archivesuburls.sh
@@ -3,12 +3,9 @@
 if [ $2 == "time" ]
 then
 
-
-echo -n "Enter max time to wait in secconds and press [ENTER]: "
-read usrinput
 mkdir $1; cd $1; 
-curl "http://web.archive.org/cdx/search/cdx?url=*.$1/*&output=text&fl=original&collapse=urlkey" --max-time $usrinput >> "wayback-urls-$usrinput.out" # Get urls from archieve.org 
-grep -Po '.*?//\K.*?(?=/)' "wayback-urls-$usrinput.out" | sort -u >> "wayback-subdomains-$usrinput.out"
+curl "http://web.archive.org/cdx/search/cdx?url=*.$1/*&output=text&fl=original&collapse=urlkey" --max-time $3 >> "wayback-urls-$3.out" # Get urls from archieve.org 
+grep -Po '.*?//\K.*?(?=/)' "wayback-urls-$3.out" | sort -u >> "wayback-subdomains-$3.out"
 
 else 
 
